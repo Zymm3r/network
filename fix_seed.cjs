1959d97de5945a6906b0a159912da40118a64e53
Zymm3r <164781821+Zymm3r@users.noreply.github.com>
2026-06-05 11:52:14 +0700

    Delete fix_seed.cjs

--- fix_seed.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let sql = fs.readFileSync('src/app/data/seed.sql', 'utf8');
-sql = sql.replace(/('ccna001-\d+\.jpg'|'wireshark-\d+\.jpg'|'python-\d+\.jpg'|'sec-\d+\.jpg'|'adv-\d+\.jpg')/g, (match) => {
-    return match.replace("'", "'lesson-thumbnails/");
-});
-fs.writeFileSync('src/app/data/seed.sql', sql);
-console.log('Done');
