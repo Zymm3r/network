df8fbc133d40b2f51ddf8cd95e1aa9fdcd940f3d
unknown <tharathaep3@gmail.com>
2026-06-05 11:43:44 +0700

    chore: remove unused npm dependencies and patch security vulnerabilities - removes 8 unused packages (date-fns, tw-animate-css, dotenv, prop-types, storybook addons), patches react-router and vite CVEs, verified zero regressions

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -3,7 +3,4 @@ export { useCourses, useCourse } from './useCourses';
 export { useLessons, useLesson } from './useLessons';
 export { useEnrollments, useProgress, useCourseProgress, useLessonProgress, useLessonsProgress } from './useProgress';
 export { usePaths, usePath } from './usePaths';
-export { useResources, useResource } from './useResources';
-export { useCertificates, useUserCertificate } from './useCertificates';
-export { useBookmarks } from './useBookmarks';
-export { useExercises, useExerciseSubmit } from './useExercises';
+
